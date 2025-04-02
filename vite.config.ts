3c90eb7531e54aaefe885fd9726bb00ba52f240f
Kishan-Jaiswar <jaiswarkishan78@gmail.com>
2025-04-02 22:30:49 +0530

    fix:vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "./", // ✅ Fix for Vercel routing
   build: {
     outDir: "dist", // ✅ Ensures output in "dist"
